349210abffa135b28b0de27ff55e51df08dde48d
Linkw3b <manuel.taraud@gmail.com>
2015-10-19 21:55:22 +0200

    Mobile Resolution : Close Nav when resize

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,23 +8,20 @@ jQuery(function() {
     /* Listeners */
     jQuery(document).on('scroll', { elem: jQuery('#'+header) }, headerFixed);
 
+    jQuery(window).on('resize', function() {
+        var nav = jQuery("#"+header).next();
+        if(nav.hasClass('js-expanded')) {
+            var scroll = jQuery(document).scrollTop();
+            var top = jQuery('body').css('top');
+            toggleNav(nav, scroll, top, header, main_nav_id);
+        }
+    });
+
     jQuery('#'+nav_icon_id+', .'+nav_links_class).on('click', function(event) {
-        var nav = jQuery(event.currentTarget).parent().next();
+        var nav = jQuery(event.currentTarget).parents('#'+header).next();
         var scroll = jQuery(document).scrollTop();
         var top = jQuery('body').css('top');
-        if(nav.hasClass('expanded')) {
-            jQuery('#'+main_nav_id).animate({left: '-80%'});
-            jQuery('body, #'+header).animate({left: '0'}, function() {
-                var scrollBody = -parseInt(top.split('px')[0]);
-                jQuery('body').css({position: '', top: '', left: ''})
-                jQuery(document).scrollTop(scrollBody);
-            });
-        } else {
-            jQuery('body').css({position: 'fixed', top: -scroll});
-            jQuery('body, #'+header).animate({left: '80%'});
-            jQuery('#'+main_nav_id).animate({left: '0'});
-        }
-        nav.toggleClass('expanded');
+        toggleNav(nav, scroll, top, header, main_nav_id);
     });
 });
 
@@ -34,4 +31,20 @@ function headerFixed(event) {
     } else {
         event.data.elem.removeClass('fixed');
     }
+}
+
+function toggleNav(nav, scroll, top, header, main_nav_id) {
+    if(nav.hasClass('js-expanded')) {
+        jQuery('#'+main_nav_id).animate({left: '-80%'});
+        jQuery('body, #'+header).animate({left: '0'}, function() {
+            var scrollBody = -parseInt(top.split('px')[0]);
+            jQuery('body').css({position: '', top: '', left: ''})
+            jQuery(document).scrollTop(scrollBody);
+        });
+    } else {
+        jQuery('body').css({position: 'fixed', top: -scroll});
+        jQuery('body, #'+header).animate({left: '80%'});
+        jQuery('#'+main_nav_id).animate({left: '0'});
+    }
+    nav.toggleClass('js-expanded');
 }
