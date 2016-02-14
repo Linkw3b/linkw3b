94aeebce46f5676368ff4434befcac11391ec7ab
Linkw3b <manuel.taraud@gmail.com>
2016-02-14 14:17:46 +0100

    Isolate lazydisplay + Improvement with hiding

---
+++ app/scripts/jquery.lazydisplay.js
@@ -0,0 +1,134 @@
+var LazyDisplay = function(lazy_display_elements, lazy_displayed_class) {
+    this.lazy_display_elements = lazy_display_elements;
+    this.lazy_displayed_class = lazy_displayed_class;
+
+    jQuery(window).on('resize, scroll', function() {
+        updateElementsDisplayed(lazy_display_elements, lazy_displayed_class)
+    });
+
+    updateElementsDisplayed(lazy_display_elements, lazy_displayed_class);
+
+    function updateElementsDisplayed(lazy_display_elements, lazy_displayed_class) {
+        var elements_to_display = new Array();
+        var elements_to_hide = new Array();
+        jQuery.each(jQuery('.'+lazy_display_elements), function(index, elem) {
+            if(jQuery(elem).hasClass(lazy_displayed_class)) {
+                if(!isElementToDisplay(elem)) {
+                    elements_to_hide.push(elem);
+                }
+            } else {
+                if(isElementToDisplay(elem)) {
+                    elements_to_display.push(elem);
+                }
+            }
+        });
+        jQuery.each(elements_to_display, function(index, elem) {
+            var duration = index*100;
+            jQuery(elem).delay(duration).queue(function() {
+                displayElement(jQuery(elem), lazy_displayed_class);
+                jQuery(elem).addClass(lazy_displayed_class).dequeue();
+            });
+        });
+        jQuery.each(elements_to_hide, function(index, elem) {
+            var duration = index*100;
+            jQuery(elem).delay(duration).queue(function() {
+                hideElement(jQuery(elem), lazy_displayed_class);
+                jQuery(elem).removeClass(lazy_displayed_class).dequeue();
+            });
+        });
+    }
+
+    function isElementToDisplay(elem) {
+        var screenHeight = jQuery(window).height();
+        var screenWidth = jQuery(window).width();
+        var scrollPosition = jQuery(window).scrollTop();
+        var windowCoords = getCoords(window);
+        var elemCoords = getCornersCoords(elem);
+        var elemInScreen = false;
+
+
+        jQuery.each(elemCoords, function(index, corner) {
+            if( corner.x >= windowCoords.left &&
+                corner.x <= windowCoords.width &&
+                corner.y >= windowCoords.top &&
+                corner.y <= windowCoords.height)
+            {
+                elemInScreen = true;
+            }
+        });
+
+        return elemInScreen
+    }
+
+    function getCoords(element) {
+        var elem = jQuery(element);
+        var elemHeight = elem.height();
+        var elemWidth = elem.width();
+        var scrollPosition = jQuery(window).scrollTop();
+        var elemPosition = {
+            'top': 0,
+            'left': 0
+        }
+        if(!jQuery.isWindow(element)) {
+            elemPosition = elem.offset();
+        } else {
+            scrollPosition = 0;
+        }
+        var elemCoords = {
+            'top': elemPosition.top - scrollPosition,
+            'width': elemPosition.left + elemWidth,
+            'height': elemPosition.top + elemHeight - scrollPosition,
+            'left': elemPosition.left
+        };
+        return elemCoords;
+    }
+
+    function getCornersCoords(element) {
+        var elem = jQuery(element);
+        var elemHeight = elem.height();
+        var elemWidth = elem.width();
+        var scrollPosition = jQuery(window).scrollTop();
+        var elemPosition = {
+            'top': 0,
+            'left': 0
+        }
+        if(!jQuery.isWindow(element)) {
+            elemPosition = elem.offset();
+        } else {
+            scrollPosition = 0;
+        }
+        var elemCoords = {
+            'cornerTopLeft': {
+                'x': elemPosition.left,
+                'y': elemPosition.top-scrollPosition
+            },
+            'cornerTopRight': {
+                'x': elemPosition.left + elemWidth,
+                'y': elemPosition.top - scrollPosition
+            },
+            'cornerBottomLeft': {
+                'x': elemPosition.left,
+                'y': elemPosition.top + elemHeight - scrollPosition
+            },
+            'cornerBottomRight': {
+                'x': elemPosition.left + elemWidth,
+                'y': elemPosition.top + elemHeight - scrollPosition
+            }
+        };
+        return elemCoords;
+    }
+
+    function displayElement(elem, displayedClass) {
+        elem.css({
+            'top': '0',
+            'opacity': '1'
+        });
+    }
+
+    function hideElement(elem, displayedClass) {
+        elem.css({
+            'top': '30px',
+            'opacity': '0'
+        });
+    }
+}
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,10 +5,9 @@ jQuery(function() {
         nav_icon_id = 'nav-icon',
         nav_links_class = 'js-nav-link',
         nav_expanded_class = 'js-expanded',
-        lazy_display_elements = 'js-lazy-display',
-        lazy_displayed_class = 'js-displayed',
         progress_points = 'js-points',
         ripple_elements = '.button, .portfolio-overlay, .nav-link, .footer-link';
+        lazy_display = new LazyDisplay('js-lazy-display', 'js-displayed');
 
     /* Listeners */
     jQuery(document).on('scroll', { elem: jQuery('#'+header) }, headerFixed);
@@ -20,7 +19,6 @@ jQuery(function() {
             var top = jQuery('body').css('top');
             toggleNav(nav, scroll, top, header, main_nav_id, nav_expanded_class);
         }
-        updateElementsDisplayed(lazy_display_elements, lazy_displayed_class)
     });
 
     jQuery('#'+nav_icon_id+', .'+nav_links_class).on('click', function(event) {
@@ -34,12 +32,6 @@ jQuery(function() {
 
     jQuery(ripple_elements).on('click', function(event) {
         rippleEffect(this, event);
-    });
-
-    updateElementsDisplayed(lazy_display_elements, lazy_displayed_class);
-
-    jQuery(document).on('scroll', function(event) {
-        updateElementsDisplayed(lazy_display_elements, lazy_displayed_class)
     });
 
     setInterval( function() {
@@ -97,111 +89,6 @@ function rippleEffect(elem, event) {
     y = event.pageY - parent.offset().top - ripple.height()/2;
 
     ripple.css({top: y+'px', left: x+'px'}).addClass("animate");
-}
-
-function updateElementsDisplayed(lazy_display_elements, lazy_displayed_class) {
-    var elements = new Array();
-    jQuery.each(jQuery('.'+lazy_display_elements), function(index, elem) {
-        if(!jQuery(elem).hasClass(lazy_displayed_class)) {
-            if(isElementToDisplay(elem)) {
-                elements.push(elem);
-            }
-        }
-    });
-    jQuery.each(elements, function(index, elem) {
-        var duration = index*100;
-        jQuery(elem).delay(duration).queue(function() {
-            displayElement(jQuery(elem), lazy_displayed_class);
-            jQuery(elem).addClass(lazy_displayed_class).dequeue();
-        });
-    });
-}
-
-function isElementToDisplay(elem) {
-    var screenHeight = jQuery(window).height();
-    var screenWidth = jQuery(window).width();
-    var scrollPosition = jQuery(window).scrollTop();
-    var windowCoords = getCoords(window);
-    var elemCoords = getCornersCoords(elem);
-    var elemInScreen = false;
-
-
-    jQuery.each(elemCoords, function(index, corner) {
-        if( corner.x >= windowCoords.left &&
-            corner.x <= windowCoords.width &&
-            corner.y >= windowCoords.top &&
-            corner.y <= windowCoords.height)
-        {
-            elemInScreen = true;
-        }
-    });
-
-    return elemInScreen
-}
-
-function getCoords(element) {
-    var elem = jQuery(element);
-    var elemHeight = elem.height();
-    var elemWidth = elem.width();
-    var scrollPosition = jQuery(window).scrollTop();
-    var elemPosition = {
-        'top': 0,
-        'left': 0
-    }
-    if(!jQuery.isWindow(element)) {
-        elemPosition = elem.offset();
-    } else {
-        scrollPosition = 0;
-    }
-    var elemCoords = {
-        'top': elemPosition.top - scrollPosition,
-        'width': elemPosition.left + elemWidth,
-        'height': elemPosition.top + elemHeight - scrollPosition,
-        'left': elemPosition.left
-    };
-    return elemCoords;
-}
-
-function getCornersCoords(element) {
-    var elem = jQuery(element);
-    var elemHeight = elem.height();
-    var elemWidth = elem.width();
-    var scrollPosition = jQuery(window).scrollTop();
-    var elemPosition = {
-        'top': 0,
-        'left': 0
-    }
-    if(!jQuery.isWindow(element)) {
-        elemPosition = elem.offset();
-    } else {
-        scrollPosition = 0;
-    }
-    var elemCoords = {
-        'cornerTopLeft': {
-            'x': elemPosition.left,
-            'y': elemPosition.top-scrollPosition
-        },
-        'cornerTopRight': {
-            'x': elemPosition.left + elemWidth,
-            'y': elemPosition.top - scrollPosition
-        },
-        'cornerBottomLeft': {
-            'x': elemPosition.left,
-            'y': elemPosition.top + elemHeight - scrollPosition
-        },
-        'cornerBottomRight': {
-            'x': elemPosition.left + elemWidth,
-            'y': elemPosition.top + elemHeight - scrollPosition
-        }
-    };
-    return elemCoords;
-}
-
-function displayElement(elem, displayedClass) {
-    elem.css({
-        'top': '0',
-        'opacity': '1'
-    });
 }
 
 function progressPoints(elements_class) {
